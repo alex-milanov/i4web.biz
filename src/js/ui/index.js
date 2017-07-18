5d1937196abe9047d5ff3273b208068d515b1fd1
Alex Milanov <alex@i4web.biz>
2017-07-18 13:54:22 +0300

    initial project setup, ui and landing page

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 // dom
-const {section, button, span} = require('iblokz-snabbdom-helpers');
+const {
+	section, button, span, div, a, p,
+	h1, h2, ul, li, i
+} = require('iblokz-snabbdom-helpers');
 // components
 const counter = require('./counter');
 
-module.exports = ({state, actions}) => section('#ui', [
-	counter({state, actions})
-]);
+module.exports = ({state, actions}) => section('#ui', div('.hero', [
+	h1('Tailor-made crafted web and multi-platform experiences'),
+	h2([
+		'...:: i4web.biz .:::...:.. ::...::.. ',
+		span('.small', [
+			i('.small.fa.fa-envelope-o'),
+			' alex',
+			i('.small.fa.fa-at'),
+			'i4web.biz '
+		]),
+		' ....::...::: ...:: '
+	])
+]));
---
+++ src/wiki/attr.md
@@ -0,0 +1,7 @@
+# Some attributions here
+- https://unsplash.com/photos/p-rN-n6Miag
+- https://unsplash.com/photos/Mn9Fa_wQH-M
+- https://unsplash.com/photos/jJyN5k-BRqY - tanya
+- https://unsplash.com/photos/zjTTu8EtqZ4 - rich
+- https://unsplash.com/photos/Dn3ATeXQEQ4 - joshua
+- https://unsplash.com/photos/ylQBibJByto - raphael
